3ead0e031b73f807554a5b28fad1fc9ea77e18bd
Henrique Minillo <henriqueminillo@hotmail.com>
2024-07-24 20:43:08 -0300

    Enviando arquivos do curso JS

---
+++ 4_string/script.js
@@ -0,0 +1,8 @@
+console.log("Oi, meu nome é Henrique");
+console.log('Teste');
+console.log(`Teste`);
+
+console.log(typeof"Oi, meu nome é Henrique");
+console.log(typeof'Teste');
+console.log(typeof`Teste`);
+
