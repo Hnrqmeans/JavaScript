fbd7f1da2936f0c071a59c996ee3866feb8ca557
Henrique Minillo <henriqueminillo@hotmail.com>
2024-07-25 19:33:48 -0300

    Enviando arquivos do curso JS

---
+++ Aulas/1_Introdução/script.js
@@ -0,0 +1,1 @@
+console.log(" Hello Word do Script.js! ");
---
+++ Aulas/5_string_avancado/script.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \nSegunda linha");
+console.log("'Hello Word' do 'Script.js!'");
+console.log('"Hello Word" do "Script.js!"');
+console.log(`A multiplicação de 5 por 3 é ${5*3}`);
+console.log("O "+"Meu "+"Nome "+"É "+"Henrique" );
