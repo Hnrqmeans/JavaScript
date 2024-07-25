5303921c1ea05542b7b9477a66993fd7c5306147
Henrique Minillo <henriqueminillo@hotmail.com>
2024-07-25 19:35:11 -0300

    Enviando arquivos do curso JS

---
+++ Exercicios/1_Exercicio_01 copy/script.js
@@ -0,0 +1,5 @@
+// Escreva três valores em string em um arquivo e exiba no navegador com console.log();
+
+console.log('Aspas simples');
+console.log("Aspas duplas");
+console.log(`Template litrals ${1+1}`);
