2d467b297c55fb1830c55c262b15c758a9f0acba
Henrique Minillo <henriqueminillo@hotmail.com>
2024-07-24 22:34:00 -0300

    Atualizando as aulas

---
+++ 9_boolena_op_ternario/script.js
@@ -0,0 +1,7 @@
+// condição ? expressão_se_verdadeira : expressão_se_falsa
+
+console.log(5 > 2 ? 'è sim' : 'é não');
+
+console.log(true ? 5 : 4);
+
+console.log("Henrique" == "Henrique" ? "Olá, Henrique" : "Não é o Henrique!");
