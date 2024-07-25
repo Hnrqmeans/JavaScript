20e7d7fa83e7e1dc36433656c6826f0c5fb506f5
Henrique Minillo <henriqueminillo@hotmail.com>
2024-07-24 21:28:38 -0300

    Enviando arquivos do curso JS

--- 6_bolean comparacao/script.js
+++
@@ -1,8 +0,0 @@
-console.log(1 > 2);
-console.log(5 < 10);
-console.log(3 >= 3);
-console.log(5 <= 4);
-console.log(5 == 4);
-console.log('Henrique' != 'Henrique');
-console.log(3 === '3');
-console.log(4 === 4);
